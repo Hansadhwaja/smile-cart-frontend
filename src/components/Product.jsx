0173b2f0f6e399455f08a4507ce14cbe6e6cb298
Hansadhwaja <hansadhwajabiswal@gmail.com>
2025-02-27 16:31:42 +0530

    Add Typography to Product Page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,14 @@
+import { Typography } from "neetoui";
+
 import Carousel from "./Carousel";
 import { IMAGE_URLS } from "./constants";
 
 const Product = () => (
   <div className="px-6 pb-6">
     <div>
-      <p className="py-2 text-4xl font-semibold">Infinix INBOOK</p>
+      <Typography className="py-2 text-4xl font-semibold">
+        Infinix INBOOK
+      </Typography>
       <hr className="border-2 border-black" />
     </div>
     <div className="mt-6 flex gap-4">
@@ -12,12 +16,12 @@ const Product = () => (
         <Carousel imageUrls={IMAGE_URLS} title="Infinix Inbook" />
       </div>
       <div className="w-3/5 space-y-4">
-        <p>
+        <Typography>
           Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey - 1 Year Warranty.
-        </p>
-        <p>MRP: $395.97</p>
-        <p className="font-semibold">Offer price: $374.43</p>
-        <p className="font-semibold text-green-600">6% off</p>
+        </Typography>
+        <Typography>MRP: $395.97</Typography>
+        <Typography className="font-semibold">Offer price: $374.43</Typography>
+        <Typography className="font-semibold text-green-600">6% off</Typography>
       </div>
     </div>
   </div>
