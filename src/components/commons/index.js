e60c373790abfc4a4b7a3f387f0ae18f41f20998
Hansadhwaja <hansadhwajabiswal@gmail.com>
2025-03-01 21:50:31 +0530

    Added ability to set quantity of cart items

--- src/components/commons/index.js
+++ src/components/commons/index.js
@@ -2,5 +2,14 @@ import AddToCart from "./AddToCart";
 import Header from "./Header";
 import PageLoader from "./PageLoader";
 import PageNotFound from "./PageNotFound";
+import ProductQuantity from "./ProductQuantity";
+import TooltipWrapper from "./TooltipWrapper";
 
-export { Header, PageNotFound, PageLoader, AddToCart };
+export {
+  Header,
+  PageNotFound,
+  PageLoader,
+  AddToCart,
+  ProductQuantity,
+  TooltipWrapper,
+};
